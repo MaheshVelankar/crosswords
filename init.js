b711804344c1d46e4389d901802387ae27af62a8
MaheshVelankar <mvelanka@gmail.com>
2026-02-26 11:51:13 -0500

    Update JSON request handling in init.js
    Refactor JSON request handling and improve error logging.

--- init.js
+++ init.js
@@ -1,10 +1,27 @@
 const paramsString = window.location.search;
 const searchParams = new URLSearchParams(paramsString);
 const node = searchParams.get("cw");
+
 if (node === null || node == '') {
     console.error('query param cw not found');
     return;
 }
-$.getJSON( node+".json", function( json ) {
-    console.log( "$ JSON Data received, name is " + json.name);
-});
+
+const cwFile = node + '.json';
+
+$.getJSON(cwFile)
+    .done(function(data) {
+        // Success: data is the parsed JSON object
+        console.log( "$ JSON Data received, name is " + json.name);
+        //console.log("Success:", data);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        // Error: this function is called if the request fails
+        console.error("Error:", textStatus, errorThrown);
+        console.log("Response Text:", jqXHR.responseText); // Contains the raw response text
+    })
+    .always(function() {
+        // This function will always run, regardless of success or failure
+        console.log("Request complete.");
+    });
+
