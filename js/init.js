cc5c788caebf56ecb546fbfe358b5e6e88dcab94
Mahesh Velankar <mvelanka@gmail.com>
2026-02-26 15:38:18 -0500

    json path correction

--- js/init.js
+++ js/init.js
@@ -7,7 +7,7 @@ if (node === null || node == '') {
     node = '_no_data_';
 }
 
-const cwFile = '/data/' + node + '.json';
+const cwFile = 'data/' + node + '.json';
 
 $.getJSON(cwFile)
     .done(function(data) {
