111adaa1ea0ca63a77aa43477333ce8e1ff8e715
MaheshVelankar <mvelanka@gmail.com>
2026-02-26 11:22:47 -0500

    Check for 'cw' query parameter and log error
    Add error handling for missing 'cw' query parameter

--- init.js
+++ init.js
@@ -1,6 +1,10 @@
 const paramsString = window.location.search;
 const searchParams = new URLSearchParams(paramsString);
 const node = searchParams.get("cw");
+if (node == '') {
+    console.error('query param cw not found');
+    return;
+}
 $.getJSON( node+".json", function( json ) {
     console.log( "$ JSON Data received, name is " + json.name);
 });
